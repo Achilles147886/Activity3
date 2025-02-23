622c4728b23d052ce16b16acc768427e3e7668c7
Laguador,Achilles S. <achilleslaguador14723@gmail.com>
2025-02-23 22:56:03 +0800

    Add files via upload

---
+++ js.js
@@ -0,0 +1,5 @@
+function changeText() {
+    document.getElementById("message").innerText = "Text changed!";
+    messageElement.style.color = "#ff7e5f";
+    messageElement.style.fontWeight = "bold";
+    }
